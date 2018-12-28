5fb8ec2360d52fac6e700b45c369d9c33abd512c
Thomas Schildhauer <tschildhauer@serva-ts.com>
2018-12-28 12:01:33 +0100

    wip add prime numbers class

---
+++ Games.Plugin.Sudoku/GamePlan/Compare/PrimeNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games.Plugin.Sudoku.GamePlan.Compare
+{
+    public class PrimeNumbers
+    {
+        private List<int> _primeNumbers;
+
+        public PrimeNumbers()
+        {
+
+        }
+
+        public List<int> CalculatePrimeNumbers(int maxNumber)
+        {
+
+            return _primeNumbers;
+        }
+
+    }
+}
