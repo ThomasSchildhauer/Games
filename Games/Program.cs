8c91519908b80aba141be66d0aa80caecd6ad8f3
Thomas Schildhauer <tschildhauer@serva-ts.com>
2019-04-23 01:07:24 +0200

    Added Games project (Console App) as basic start project of http client, plugin system and UI trigger

---
+++ Games/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games
+{
+    class Program
+    {
+        //here comes the plugin system the GamesUI trigger and the http client
+        static void Main(string[] args)
+        {
+        }
+    }
+}
